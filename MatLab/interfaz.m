758c0bb6bbaa17fed3bfa85923b9838c87202e03
mko010 <33830005+mko010@users.noreply.github.com>
2018-02-12 14:52:51 +0100

    first commit

---
+++ MatLab/interfaz.m
@@ -0,0 +1,105 @@
+function varargout = interfaz(varargin)
+% INTERFAZ MATLAB code for interfaz.fig
+%      INTERFAZ, by itself, creates a new INTERFAZ or raises the existing
+%      singleton*.
+%
+%      H = INTERFAZ returns the handle to a new INTERFAZ or the handle to
+%      the existing singleton*.
+%
+%      INTERFAZ('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in INTERFAZ.M with the given input arguments.
+%
+%      INTERFAZ('Property','Value',...) creates a new INTERFAZ or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before interfaz_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to interfaz_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help interfaz
+
+% Last Modified by GUIDE v2.5 08-Feb-2018 14:51:57
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @interfaz_OpeningFcn, ...
+                   'gui_OutputFcn',  @interfaz_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before interfaz is made visible.
+function interfaz_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to interfaz (see VARARGIN)
+
+% Choose default command line output for interfaz
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes interfaz wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = interfaz_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+% --------------------------------------------------------------------
+function Untitled_1_Callback(hObject, eventdata, handles)
+% hObject    handle to Untitled_1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+points = [];
+[x, y] = getpts;
+
+
+% --- Executes on button press in load.
+function load_Callback(hObject, eventdata, handles)
+% hObject    handle to load (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+ [filename pathname] = uigetfile({'*.jpg';'*.bmp';'*.png'},'File Selector');
+ handles.myImage = strcat(pathname, filename);
+ axes(handles.image);
+ imshow(handles.myImage)
+ set(handles.edit1,'string',filename);
+ set(handles.edit2,'string',image);
+
+ % save the updated handles object
+ guidata(hObject,handles);
+
+
+% --- Executes on button press in test.
+function test_Callback(hObject, eventdata, handles)
+% hObject    handle to test (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
